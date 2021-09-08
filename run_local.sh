6625f55682800efef3b9b3f35bf971dd8b749905
Ognjen Maric <ognjen.maric@dfinity.org>
2021-09-08 17:02:52 +0200

    Automating local deployment + updated README

---
+++ run_local.sh
@@ -0,0 +1,26 @@
+#!/user/bin/env bash
+
+dfx start --background
+git submodule add https://github.com/dfinity/internet-identity
+cd internet-identity
+npm install
+II_ENV=development dfx deploy --no-wallet --argument '(null)'
+II_CANISTER_ID=$(dfx canister --no-wallet id internet_identity)
+cd ..
+# TODO: echo the identity into local_canisters.json
+cat <<EOF >> local_canisters.json
+   {
+    "II_LOCAL_UI_CANISTER_ID": "$II_CANISTER_ID"
+   }
+EOF
+npm install
+dfx deploy
+npm start
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     opener=xdg-open;;
+    Darwin*)    opener=open;;
+esac
+
+$opener http://localhost:3000
